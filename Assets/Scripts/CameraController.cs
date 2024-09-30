cc5daa8a3e5ab9108ad51641638e3fd03d680f5e
Pedro Tozzi de Avila <techerajathiago@gmail.com>
2024-09-30 18:22:10 -0300

    :construction: Camera Controlling

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,119 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class CameraController : Singleton<CameraController>
 {
-    [SerializeField]private Camera m_camera;
-    [SerializeField] private List<Vector3> m_cameraSpawnPositionList;
-    [SerializeField] private List<Quaternion> m_cameraSpawnRotationList;
+    [SerializeField] private Camera m_camera;
+    private int m_cameraIndex;
+    [SerializeField] private CameraState m_currentState = CameraState.HAND;
+    [SerializeField] private CameraAnimConfig[] m_animConfigs;
+
+    private void Start()
+    {
+        GameInput.Instance.OnButtonVerticalDown += GameInput_Down;
+        GameInput.Instance.OnButtonVerticalUp += GameInput_Up;
+    }
+
+    public void GameInput_Up(object sender, EventArgs p_eventArgs)
+    {
+        switch (m_currentState)
+        {
+            case CameraState.HAND:
+                if (m_cameraAnimCoroutine != null) StopCoroutine(m_cameraAnimCoroutine);
+                m_cameraAnimCoroutine = StartCoroutine(AnimCamera(CameraState.TABLE));
+                break;
+        }
+    }
+
+    public void GameInput_Down(object sender, EventArgs p_eventArgs)
+    {
+        switch (m_currentState)
+        {
+            case CameraState.TABLE:
+                if (m_cameraAnimCoroutine != null) StopCoroutine(m_cameraAnimCoroutine);
+                m_cameraAnimCoroutine = StartCoroutine(AnimCamera(CameraState.HAND));
+                break;
+        }
+    }
+
+    Coroutine m_cameraAnimCoroutine;
+    Vector3 l_tempRotation, l_initialRotation, l_finalRotation, l_initialPosition, l_tempPosition, l_finalPosition;
+    CameraAnimData m_cameraData;
+    IEnumerator AnimCamera(CameraState p_state)
+    {
+        if (m_currentState == p_state) yield break;
+        m_currentState = p_state;
+
+        l_initialPosition = m_camera.transform.localPosition;
+        m_cameraData = m_animConfigs[m_cameraIndex].GetAnimData(p_state);
+        l_finalPosition = m_cameraData.LocalPosition;
+        l_initialRotation = m_camera.transform.localRotation.eulerAngles;
+        l_finalRotation = m_cameraData.LocalRotation;
+
+        float l_animTime = m_animConfigs[m_cameraIndex].AnimTime;
+        float l_time = 0f;
+
+        while (l_time <= l_animTime)
+        {
+            float l_evaluate = l_time / l_animTime;
+            l_tempPosition = Vector3.LerpUnclamped(l_initialPosition, l_finalPosition, m_cameraData.PositionCurve.Evaluate(l_evaluate));
+
+            l_tempRotation.x = Mathf.LerpAngle(l_initialRotation.x, l_finalRotation.x, m_cameraData.RotationCurve.Evaluate(l_evaluate));
+            l_tempRotation.y = Mathf.LerpAngle(l_initialRotation.y, l_finalRotation.y, m_cameraData.RotationCurve.Evaluate(l_evaluate));
+            l_tempRotation.z = Mathf.LerpAngle(l_initialRotation.z, l_finalRotation.z, m_cameraData.RotationCurve.Evaluate(l_evaluate));
+
+            m_camera.transform.localPosition = l_tempPosition;
+            m_camera.transform.localRotation = Quaternion.Euler(l_tempRotation);
+
+            l_time += Time.deltaTime;
+            yield return null;
+        }
+
+        l_tempPosition = Vector3.LerpUnclamped(l_initialPosition, l_finalPosition, 1f);
+
+        l_tempRotation.x = Mathf.LerpAngle(l_initialRotation.x, l_finalRotation.x, 1f);
+        l_tempRotation.y = Mathf.LerpAngle(l_initialRotation.y, l_finalRotation.y, 1f);
+        l_tempRotation.z = Mathf.LerpAngle(l_initialRotation.z, l_finalRotation.z, 1f);
+
+        m_camera.transform.SetLocalPositionAndRotation(l_tempPosition, Quaternion.Euler(l_tempRotation));
+
+        m_cameraAnimCoroutine = null;
+    }
+
 
     public void SetCamera(int p_index)
     {
-        m_camera.transform.SetPositionAndRotation(m_cameraSpawnPositionList[p_index], m_cameraSpawnRotationList[p_index]);
+        m_cameraIndex = p_index;
+        m_camera.transform.SetLocalPositionAndRotation(
+            m_animConfigs[p_index].GetAnimData(CameraState.HAND).LocalPosition,
+               Quaternion.Euler(m_animConfigs[p_index].GetAnimData(CameraState.HAND).LocalRotation));
     }
 
+#if UNITY_EDITOR
+    public void SaveCurrentAnimData()
+    {
+        m_animConfigs[0].SetAnimData(m_currentState, m_camera.transform.localPosition, m_camera.transform.localEulerAngles);
+        EditorUtility.SetDirty(m_animConfigs[0]);
+    }
+#endif
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(CameraController))]
+public class CameraControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Save"))
+        {
+            (target as CameraController).SaveCurrentAnimData();
+        }
+    }
+}
+#endif
--- Assets/Scripts/Game/GameInputs.cs
+++ Assets/Scripts/Game/GameInputs.cs
@@ -1,37 +1,49 @@
 using System;
 using UnityEngine;
 
-public class GameInput : Singleton<GameInput> {
+public class GameInput : Singleton<GameInput>
+{
 
     public event EventHandler OnInteractAction;
     public event EventHandler OnStopInteractAction;
     public event EventHandler OnPauseAction;
     public event EventHandler OnMoveMouse;
+    public event EventHandler OnButtonVerticalUp;
+    public event EventHandler OnButtonVerticalDown;
 
     Vector3 m_lastMousePosition;
-	void Update()
-	{
-		if (Input.GetMouseButtonDown(0))
-			Interact();
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Interact();
         if (Input.GetMouseButtonUp(0))
             StopInteract();
 
         if (Input.GetKeyDown(KeyCode.Escape))
-			Pause();
+            Pause();
 
-        if (Input.mousePosition != m_lastMousePosition) 
+        if (Input.mousePosition != m_lastMousePosition)
             MoveMouse();
-	}
 
-    private void Pause() {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            VerticalAxis(true);
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            VerticalAxis(false);
+    }
+
+    private void Pause()
+    {
         OnPauseAction?.Invoke(this, EventArgs.Empty);
     }
 
-    private void Interact() {
+    private void Interact()
+    {
         OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
-    private void StopInteract(){
+    private void StopInteract()
+    {
         OnStopInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
@@ -41,4 +53,9 @@ public class GameInput : Singleton<GameInput> {
         OnMoveMouse?.Invoke(m_lastMousePosition, EventArgs.Empty);
     }
 
+    private void VerticalAxis(bool p_up)
+    {
+        if(p_up) OnButtonVerticalUp?.Invoke(this, EventArgs.Empty);
+        else OnButtonVerticalDown?.Invoke(this,EventArgs.Empty);
+    }
 }
